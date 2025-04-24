4bef8e867e437392edcd8548302435b355bbdae1
jiwoo <jiwoook01@naver.com>
2025-04-24 11:04:11 +0900

    MidTermTest

---
+++ MidtermTest/MidTermTest/Problem1/main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+// PassOrFail 함수 정의
+int PassOrFail(int score) {
+    if (score >= 50) {
+        return 1;
+    }
+    else {
+        return 0; // Fail
+    }
+}
+
+int main() {
+
+    int myScore = 51;
+
+    int result = PassOrFail(myScore);
+
+    if (result == 1) {
+        printf("재시험 없습니다!\n");
+    }
+    else {
+        printf("우리는 망했다… 재시험이다…\n");
+    }
+
+    return 0;
+}
